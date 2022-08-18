6d3cf21d40423d16455d2793b402254eaececd91
«olga» <lilu_009@mail.ru>
2022-08-19 02:10:49 +0800

    example2

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,4 +1,12 @@
-﻿Console.WriteLine("Введите число");
-int number = Convert.ToInt32(Console.ReadLine());
-int value = number * number;
-Console.Write("квадрат числа = " +value);
+﻿Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1 == number2 * number2)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число НЕ является квадратом второго");
+}
