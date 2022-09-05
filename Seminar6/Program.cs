a75d646f0f5acd2c8d7e24b10b7a071bcaeeceb4
«olga» <lilu_009@mail.ru>
2022-09-06 03:21:33 +0800

    seminar6

---
+++ Seminar6/Program.cs
@@ -0,0 +1,224 @@
+﻿// string[,] table = new string[2, 5];
+// table[1, 2] = "слово";
+
+// for(int rows = 0; rows < 2; rows++)
+// {
+//     for(int columns = 0; columns < 5; columns++)
+//     {
+//         Console.WriteLine("-" +table[rows, columns]);
+//     }
+// }
+
+
+
+// void PrintArray(int[,] matr)
+// {
+//     for(int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             Console.Write(matr[i, j] + " ");
+//         }
+
+//         Console.WriteLine();
+//     }
+// }
+
+// void FilltArray(int[,] matr)
+// {
+//     for(int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             matr[i, j] = new Random().Next(1, 10);
+//         }
+//     }
+// }
+
+// int[,] matrix = new int[3, 4];
+
+// PrintArray(matrix);
+// FilltArray(matrix);
+// Console.WriteLine();
+// PrintArray(matrix);
+
+
+
+// int[,] pic = new int[,]
+// {
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+// {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 },
+// {0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+// {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+// };
+
+// void PrintImage(int[,] image)
+// {
+//     for(int i = 0; i < image.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < image.GetLength(1); j++)
+//         {
+//             if(image[i, j] == 0) Console.Write(" ");
+//             else Console.Write("+");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// void FillImage(int row, int col)
+// {
+//     if(pic[row, col] == 0)
+//     {
+//         pic[row, col] = 1;
+//         FillImage(row - 1, col);
+//         FillImage(row, col - 1);
+//         FillImage(row + 1, col);
+//         FillImage(row, col + 1);
+//     }
+// }
+
+// FillImage(13, 13);
+// PrintImage(pic);
+
+
+
+// Факториал
+
+// double Factorial(int n)
+// {
+//     // 1! = 1
+//     // 0! = 1
+//     if(n == 1) return 1;
+//     else return n * Factorial(n - 1);
+// }
+// Console.WriteLine(Factorial(40));
+
+
+
+// Фибоначчи
+
+// int Fibonacci(int n)
+// {
+//     if(n == 1 || n == 2) return 1;
+//     else return Fibonacci(n - 1) + Fibonacci(n - 2);
+// }
+
+// for(int i = 1; i < 10; i++)
+// {
+//     Console.Write(Fibonacci(i) +" ");
+// }
+
+
+
+// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+
+// Console.Write("Введите первое число: ");
+// int A =  Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число: ");
+// int B =  Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите третье число: ");
+// int C =  Convert.ToInt32(Console.ReadLine());
+
+// if(A < (B + C) && B < (A + C) && C < (A + B))
+// {
+//     Console.Write("Треугольник со сторонами такой длины существует");
+// }
+
+// else
+// {
+//     Console.Write("Такого треугольника нет");
+// }
+
+
+
+// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+// Console.WriteLine("Введите десятичное число: ");
+// int number = int.Parse(Console.ReadLine());
+
+// string s = string.Empty;
+
+// while(number > 0)
+// {
+//     s = Convert.ToString(number % 2) + s;
+//     number /= 2; 
+// }
+
+// Console.WriteLine(s);
+
+
+
+// Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+
+// Console.Write("Введите число N = ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int first = 0;
+// int second = 1;
+// int current = 0;
+    
+// Console.Write($"Если N = {n} ->");
+
+// for (int i = 0; i < n; i++)
+// {
+//     if (i == 0)
+//     {
+//         Console.Write(" 0");
+//     }
+//     else if (i == 1)
+//     {
+//         Console.Write(" 1");
+//     }
+//     else
+//     {
+//         current = first + second;
+//         first = second;
+//         second = current;
+
+//         Console.Write($" {current}");
+//     }
+// }
+
+
+
+// Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования. 
+
+public class ClassWork {
+    public static void Main(string[] args) {
+        CopyArray(new int[]{1, 2, 3, 4, 5});
+    }
+
+static void CopyArray(int[] array)
+{
+    Console.Write("Копия массива: ");
+
+    int[] copy = new int[array.Length];
+
+    for(int i = 0; i < array.Length; i ++)
+    {
+        copy[i] = array[i];
+        Console.Write(copy[i] +" ");
+    }
+}
+}
